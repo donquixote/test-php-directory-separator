e9653de850c9a882fef758d984280e2240127b66
Andreas Hennings <andreas@dqxtech.net>
2026-05-07 21:54:13 +0000

    ...

--- test.php
+++ test.php
@@ -1,9 +1,14 @@
 <?php
 
 var_export([
-    PHP_OS_FAMILY,
-    PHP_EOL,
-    DIRECTORY_SEPARATOR,
-    __FILE__,
-    __DIR__,
+    'PHP_OS_FAMILY' => PHP_OS_FAMILY,
+    'PHP_EOL' => PHP_EOL,
+    'json_encode(PHP_EOL)' => json_encode(PHP_EOL),
+    'DIRECTORY_SEPARATOR' => DIRECTORY_SEPARATOR,
+    '__FILE__' => __FILE__,
+    '__DIR__' => __DIR__,
+    getcwd(),
+    'realpath' => realpath(__DIR__ . '/..'),
+    tempnam(),
+    sys_get_temp_dir(),
 ]);
